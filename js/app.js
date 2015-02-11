2a8eb0ffb197eaf5d38c0331cfbcf796b2741e9d
Taylor Crane <taylorkcrane@gmail.com>
2015-02-10 22:23:01 -0500

    hold X to cool

--- js/app.js
+++ js/app.js
@@ -2,15 +2,18 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		$('.ryu-cool').hide();
 	})
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
 	})
 	.mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
 			{'left': '300px'},
@@ -24,7 +27,22 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	})
+		$('.ryu-cool').hide();
+	});
+	$(document).keydown(function(entry) {
+		if(entry.which == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}	
+	});
+	$(document).keyup(function(exit) {
+		if(exit.which == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}	
+	});
 });
 
 function playHadouken () {
